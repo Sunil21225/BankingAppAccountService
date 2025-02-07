8aedc2b3b67d5d95f5bfc03f4cbd4935cd05f774
sunil21225 <dsunilkumar1718@gmail.com>
2025-02-07 09:49:49 +0530

    added welcome message

--- AccountService/src/main/java/com/example/demo/controller/AccountController.java
+++ AccountService/src/main/java/com/example/demo/controller/AccountController.java
@@ -23,6 +23,7 @@ public class AccountController {
 
 	@PostMapping("/create")
 	public String saveAccount(@RequestBody Account account) {
+		System.out.println("welcome");
 		return service.createAccount(account);
 	}
 
